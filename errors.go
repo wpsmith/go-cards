3c4e3c33d03242382fea5d00905d599f993c27bb
Travis Smith <travis.smith@weather.com>
2017-06-19 15:49:49 -0400

    Refactor. Remove unused functions.

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-package cards
+package gocards
 
 import (
     "errors"
@@ -9,13 +9,3 @@ var (
     eDECK_HAS_NO_CARDS = errors.New("Deck has no cards")
     eDECK_WAS_NOT_CREATED = errors.New("Deck was not created")
 )
-
-// Error for Suit property already being created
-func errorCardPropertyAlreadySet(property string) error {
-    return errors.New("Card " + property + " already set")
-}
-
-// Error for Suit property already being created
-func errorCardMissingRequiredProperties(property string) error {
-    return errors.New("Suit missing required properties (" + property + ").")
-}
